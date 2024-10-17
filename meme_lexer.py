bc1df783851f8245969a89042f1440ae20f2226c
Zakiy Manigo <fashiobusiness@gmail.com>
2024-10-17 10:43:54 -0400

    Update meme_lexer.py
    changing code to not use the regex library and testing our first sample input. Code is not perfect since the "x" in "1024 x 1024" is that tokenizing as an OP.

--- meme_lexer.py
+++ meme_lexer.py
@@ -1,75 +1,80 @@
-import re
-
-# Token types
-TOKEN_TYPES = {
-    'KEYWORD': ['create', 'background', 'text', 'save', 'pictures', 'resize', 'if'],
-    'OPERATOR': ['==', '!=', '+', '-', '*', '/'],
-    'LITERAL': r'[0-9]+',  # Integer literals
-    'STRING': r'"[^"]*"',  # Strings inside double quotes
-    'IDENTIFIER': r'[a-zA-Z_][a-zA-Z_0-9]*',  # Variables, keywords, and meme element names
-    'COMMENT': r'//.*',  # Single-line comments
-    'COMMA': r',',  # Comma handling
-    'WHITESPACE': r'[ \t\n]+',  # Whitespace characters
-}
-
-class Lexer:
-    def __init__(self, code):
-        self.code = code
-        self.position = 0
+class Scanner:
+    def __init__(self, source_code):
+        self.source_code = source_code
         self.tokens = []
-
-    def tokenize(self):
-        while self.position < len(self.code):
-            matched = False
-            for token_type, pattern in TOKEN_TYPES.items():
-                if token_type == 'KEYWORD':
-                    matched = self.match_keyword()
-                elif token_type == 'OPERATOR':
-                    matched = self.match_operator()
-                else:
-                    regex = re.compile(pattern)
-                    match = regex.match(self.code, self.position)
-                    if match:
-                        value = match.group(0)
-                        if token_type != 'WHITESPACE':  # Ignore whitespace tokens
-                            self.tokens.append((token_type, value))
-                        self.position = match.end(0)
-                        matched = True
-                        break
-            if not matched:
-                raise SyntaxError(f"Unrecognized token at position {self.position}: {self.code[self.position]}")
+        self.current_state = "Start"
+        self.keywords = {"create", "meme", "background", "load", "size", "border", "style", "text", 
+                         "placement", "overlay", "count", "save", "images"}
+        self.operators = {"x"}
+        self.delimiters = {" ", "\n"}
+    
+    def scan(self):
+        index = 0
+        while index < len(self.source_code):
+            char = self.source_code[index]
+            
+            if self.current_state == "Start":
+                if char.isalpha():  # Start of keyword or identifier
+                    self.current_state = "Keyword/Identifier"
+                    start_index = index
+                    while index < len(self.source_code) and (self.source_code[index].isalpha() or self.source_code[index].isdigit() or self.source_code[index] == '_'):
+                        index += 1
+                    value = self.source_code[start_index:index]
+                    token_type = "KEYWORD" if value in self.keywords else "ID"
+                    self.tokens.append((token_type, value))
+                    self.current_state = "Start"
+                elif char.isdigit():  # Start of number
+                    self.current_state = "Number"
+                    start_index = index
+                    while index < len(self.source_code) and self.source_code[index].isdigit():
+                        index += 1
+                    value = self.source_code[start_index:index]
+                    self.tokens.append(("INT", value))
+                    self.current_state = "Start"
+                elif char == '"':  # Start of string literal
+                    self.current_state = "String"
+                    start_index = index + 1
+                    index += 1
+                    while index < len(self.source_code) and self.source_code[index] != '"':
+                        index += 1
+                    value = self.source_code[start_index:index]
+                    index += 1  # To skip closing quote
+                    self.tokens.append(("STRING", value))
+                    self.current_state = "Start"
+                elif char in self.operators:  # Operator
+                    self.tokens.append(("OP", char))
+                    index += 1
+                    self.current_state = "Start"
+                elif char in self.delimiters:  # Delimiter
+                    index += 1  # Simply skip delimiters
+                    self.current_state = "Start"
+                else:  # Lexical error
+                    self.current_state = "Error"
+                    print(f"Lexical Error: Unexpected character '{char}'")
+                    break
         return self.tokens
 
-    def match_keyword(self):
-        for keyword in TOKEN_TYPES['KEYWORD']:
-            if self.code[self.position:].startswith(keyword) and not re.match(r'[a-zA-Z_0-9]', self.code[self.position + len(keyword):]):
-                self.tokens.append(('KEYWORD', keyword))
-                self.position += len(keyword)
-                return True
-        return False
+# Example usage:
+source_code = """
+create meme
+    background blue
+    load pictures dogs cats grass
+    size 1024 x 1024
+    border solid
+        color
+    style collage
+    text "we all love animals"
+        placement middle
+        overlay yes
+    text "of course"
+        placement bottom
+        overlay no
+    count 50
+save images
+"""
 
-    def match_operator(self):
-        for operator in TOKEN_TYPES['OPERATOR']:
-            if self.code[self.position:].startswith(operator):
-                self.tokens.append(('OPERATOR', operator))
-                self.position += len(operator)
-                return True
-        return False
+scanner = Scanner(source_code)
+tokens = scanner.scan()
 
-# Example function to test lexer
-def run_lexer(file_name):
-    with open(file_name, 'r') as file:
-        code = file.read()
-    lexer = Lexer(code)
-    tokens = lexer.tokenize()
-    for token in tokens:
-        print(f"<{token[0]}, {token[1]}>")
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) != 2:
-        print("Usage: python meme_lexer.py <input_file>")
-        sys.exit(1)
-    
-    file_name = sys.argv[1]
-    run_lexer(file_name)
+for token in tokens:
+    print(token)
