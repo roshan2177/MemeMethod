296ae615ac138862ccbb5a948b79c3dc93e9aa35
Zakiy Manigo <fashiobusiness@gmail.com>
2024-11-11 11:42:59 -0500

    parser has both the lexer and parser

--- run_meme_lexer.sh
+++ run_meme_lexer.sh
@@ -3,15 +3,18 @@
 # Run the lexer Python script with standard input.
 # This will execute the meme_lexer.py script using Python 3.
 # You may provide input through standard input (e.g., by piping a file).
-python3 meme_lexer.py
+
+# python3 meme_lexer.py
+# python3 meme_lexer.py 
+# sleep 2
+python3 meme_parser.py
 
 # Check if the lexer execution was successful.
 # The special variable $? holds the exit status of the last command executed.
 # If the exit status is 0, the script will print a success message.
 if [ $? -eq 0 ]; then
-    echo "Lexical analysis completed successfully."
+    echo "Syntax and lexical analysis completed successfully. AST generated."
 else
-    # If the exit status is not 0, print an error message and exit with status 1 (failure).
-    echo "Error occurred during lexical analysis."
+    echo "Error occurred during syntax or lexical analysis."
     exit 1
 fi
